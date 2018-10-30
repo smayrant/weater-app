dacf39b4d5b5924fe561cd7f0acb1b1c2bfd6fcc
Sheldrick <mrmayrant@yahoo.com>
2018-10-29 21:28:16 -0400

    added bootstrap to begin styling

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = () =>
     (
         <div>
-            <h1>Weather Finder</h1>
+            <h1 className="title">Weather Finder</h1>
             <p>Retrieve temperature, weather conditions and more</p>
         </div>
     )
